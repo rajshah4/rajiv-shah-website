216be3cca19f8d37b37567ae59de5024d1c930c9
Rajiv Shah <rshah@pobox.com>
2023-08-20 08:33:58 -0500

    updating publications

--- components/Publications.js
+++ components/Publications.js
@@ -6,12 +6,12 @@ export default function Publications() {
     <section className="bg-white dark:bg-gray-800">
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
         <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-          Publications
+        Publications
         </h1>
       </div>
       <div className="bg-[#F1F1F1] dark:bg-gray-900 -mt-4">
         <div className="grid grid-cols-1 dark:bg-gray-900 max-w-xl mx-auto pt-20">
-          {/* Publications card */}
+          {/* Experience card */}
           {userData.experience.map((exp, idx) => (
             <>
               <ExperienceCard
--- pages/publications.js
+++ pages/publications.js
@@ -2,7 +2,7 @@ import React from "react";
 import ContainerBlock from "../components/ContainerBlock";
 import Publications from "../components/Publications";
 
-export default function Publications() {
+export default function projects() {
   return (
     <ContainerBlock title="Publications - Rajiv Shah">
       <Publications />
