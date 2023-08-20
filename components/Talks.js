1cd49b995c19c47aa54758bf87917901b4d0eb99
Rajiv Shah <rshah@pobox.com>
2023-08-20 10:16:42 -0500

    keeping adding

--- components/Talks.js
+++ components/Talks.js
@@ -39,17 +39,32 @@ export default function Talks() {
           <p></p>
           <p>
           May 22, 2018, Presentation, Connected Claims - <a href='http://events.insurancenexus.com/connectedclaimsusa/conference-agenda.php'>http://events.insurancenexus.com/connectedclaimsusa/conference-agenda.php</a> 
-          </p>
-          <p>
+          </p><p>
           May 16, 2018, "Sharpen Cyber Defenses with AI and Machine Learning", SIGNAL Innovation Showcase, AFCEA, 
           <a href='http://events.afcea.org/AFCEACyberOps18/public/enter.aspx'>http://events.afcea.org/AFCEACyberOps18/public/enter.aspx</a>
-          </p>
-          <p>
+          </p><p>
           May 8, 2018, Automated AI, St. Louis, Prepare.ai - <a href='http://www.prepare.ai/conference/'>http://www.prepare.ai/conference/</a> 
-          </p>
-          <p>
+          </p><p>
           April 18, 2018, Automated AI: The Next Frontier in Analytics, Twin Cities Spark and Hadoop User Group - <a href='https://www.meetup.com/Twin-Cities-Hadoop-User-Group/events/249498506/'>https://www.meetup.com/Twin-Cities-Hadoop-User-Group/events/249498506/</a>
-          </p>
+          </p><p>
+          Feb. 15, 2018, Deep Learning (with Keras), Chicago R User Group, <a href='https://www.meetup.com/ChicagoRUG/events/247282214/'>https://www.meetup.com/ChicagoRUG/events/247282214/</a>
+          </p><p>
+          Jan. 9, 2018, Automated AI: The Next Frontier in Analytics, San Antonio Data Science Meetup - <a href='https://www.meetup.com/San-Antonio-Data-Science-Meetup/events/245251461/'>https://www.meetup.com/San-Antonio-Data-Science-Meetup/events/245251461/</a>
+          </p><p>
+          Dec. 7, 2017, Automated AI: The Next Frontier in Analytics, Houston Data Science - <a href='https://www.meetup.com/Houston-Data-Science/events/245143044/'>https://www.meetup.com/Houston-Data-Science/events/245143044/</a>
+          </p> <p>
+          Nov. 29, 2017, Automated AI: The Next Frontier in Analytics, San Antonio Data Science Meetup - <a href='https://www.meetup.com/DFW-Data-Science/events/245004965/'>https://www.meetup.com/DFW-Data-Science/events/245004965/</a>
+          </p><p>
+          Nov. 10, 2017, Automated AI: The Next Frontier in Analytics, Stampede, St. Louis - <a href='https://stampedecon.com/blog/2017/11/10/automated-ai-the-next-frontier-in-analytics-slides-from-the-stampedecon-ai-summit-2017/'>https://stampedecon.com/blog/2017/11/10/automated-ai-the-next-frontier-in-analytics-slides-from-the-stampedecon-ai-summit-2017/</a>
+          </p><p>
+          Nov. 6, 2017, Deep Learning use Keras, Open Data Science Conference - <a href='https://odsc.com/training/portfolio/deep-learning-r-using-keras'>https://odsc.com/training/portfolio/deep-learning-r-using-keras</a>
+          </p><p>
+          July 6, 2017, Keras interface for R, Toronto Deep Learning Meetup -  <a href='https://www.meetup.com/Deep-Learning-Toronto-Meetup/events/241299319/'>https://www.meetup.com/Deep-Learning-Toronto-Meetup/events/241299319/</a>
+          </p><p>
+          June 22, 2017, Keras & R, Chicago Tensorflow Meetup, <a href='https://www.meetup.com/TensorFlow-Chicago/events/240267321/'>https://www.meetup.com/TensorFlow-Chicago/events/240267321/</a>
+          </p><p>
+          Feb 3, 2017, Image Classification using Keras and Tensorflow, Champaign Data Science Group,  <a href='https://www.meetup.com/CU-DSUG/events/236236341/'>https://www.meetup.com/CU-DSUG/events/236236341/</a>
+          </p> 
       </div>
     </section>
   );
