640d5c1046f7cedb657296d28047a3674143a111
Rajiv Shah <rshah@pobox.com>
2023-08-20 20:50:09 -0500

    adding spacing

--- components/Videos.js
+++ components/Videos.js
@@ -5,7 +5,7 @@ export default function Videos() {
   return (
     <section className="bg-white dark:bg-gray-800">
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+        <h1 className=" text-5xl md:text-9xl font-bold py-25 text-center md:text-left">
           Videos
         </h1>
         Find current videos at TikTok, Instagram, or YouTube
