daacb5215443d816732c6f86524fd75aeab8a424
Rajiv Shah <rshah@pobox.com>
2025-12-31 08:40:10 -0600

    Configure next-sitemap to include blog sitemap in robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   siteUrl: 'https://rajivshah.com/',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://rajivshah.com/blog/sitemap.xml',
+    ],
+  },
 }
